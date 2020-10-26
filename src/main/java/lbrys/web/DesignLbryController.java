d09ba2e8127235021b7c9a6b087be5e6832be96c
53jk1 <bonczalke@gmail.com>
2020-10-26 22:09:39 +0100

    some bugs modified:   src/main/java/lbrys/web/DesignLbryController.java

--- src/main/java/lbrys/web/DesignLbryController.java
+++ src/main/java/lbrys/web/DesignLbryController.java
@@ -1,4 +1,47 @@
 package lbrys.web;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import lbrys.Ingredient;
+import lbrys.Ingredient.Type;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
 public class DesignLbryController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "wheat", Type.WRAP),
+                new Ingredient("COTO", "maize", Type.WRAP),
+                new Ingredient("GRBF", "ground beef", Type.PROTEIN),
+                new Ingredient("CARN", "pieces of meat", Type.PROTEIN),
+                new Ingredient("TMTO", "tomatoes diced", Type.VEGGIES),
+                new Ingredient("LETC", "lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "spicy tomato sauce", Type.SAUCE),
+                new Ingredient("SRCR", "sour cream", Type.SAUCE)
+        );
+
+        Type[] types = Ingredient.Type.values():
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase().
+                    filterByType(ingredients, type));
+        }
+        model.addAttribute("design", new Lbry());
+        return "design";
+    }
 }

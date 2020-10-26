0791f3a0a5589dc72ecb34814e517346272980f1
53jk1 <bonczalke@gmail.com>
2020-10-26 13:28:00 +0100

    Fixed tests modified:   pom.xml modified:   src/test/java/lbrys/HomeControllerTest.java

--- src/test/java/lbrys/HomeControllerTest.java
+++ src/test/java/lbrys/HomeControllerTest.java
@@ -7,12 +7,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
     @Autowired
